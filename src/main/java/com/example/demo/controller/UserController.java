9d4d33a94b494840ca316fb22f0a032cc493bd0e
Tong Ly Son <tonglyson1221998@gmail.com>
2020-11-06 10:17:43 +0700

    lan 2

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
     private String a= "commit lan 1";
+    private String b = "commit lan 2";
     private static final String USER="api/3.9/sites/507f2aa0-a887-4f27-9ef6-5a5be17e0517/users";
     private static final String GROUP="api/3.9/sites/507f2aa0-a887-4f27-9ef6-5a5be17e0517/groups";
     @Autowired
